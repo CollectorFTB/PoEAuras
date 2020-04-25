cee066099b30b26e35453dce22def5b7311a1476
Gilad David <gilad.david95@gmail.com>
2020-04-25 20:20:53 +0300

    Split tags to support_list and gem itself's tags

--- gem.py
+++ gem.py
@@ -1,18 +1,29 @@
 class SupportGem:
     def __init__(self):
         self.multiplier = 1
+    
+    def apply_to(self, active):
+        pass
 
 class Empower(SupportGem):
-    def __init__(self, level):
+    def __init__(self, level, quality=0):
+        self.name = 'Empower'
         self.level = level
-        self.tags = ['Active']
+        self.quality = quality
+        self.tags = ['Support']
+        self.support_list = ['Active']
         self.multiplier = 1.25
     
+    def apply_to(self, active):
+        active.level += self.level-1
 
 class Enlighten(SupportGem):
-    def __init__(self, level):
+    def __init__(self, level, quality=0):
+        self.name = 'Enlighten'
         self.level = level
-        self.tags = ['Active']
+        self.quality = quality
+        self.tags = ['Support']
+        self.support_list = ['Active']
     
     @property
     def multiplier(self):
